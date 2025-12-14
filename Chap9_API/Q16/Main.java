753c01c72456e50e7b793f8d5ccd6492d2a84292
root <root@takaaki.>
2025-12-15 00:32:59 +0900

    a

---
+++ Chap9_API/Q16/Main.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args){
+		String[] a = {"B","A"};
+		Stirng[] b = {"A","B"};
+		System.out.println(Arrays.compare(a,b));
+	}
+}
+
+
