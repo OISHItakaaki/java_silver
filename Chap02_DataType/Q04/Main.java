702f67658a6ed0ae1c40fa788e844ab9cde84e13
大石昂明 <takakaki.ohishi1109@gmail.com>
2025-12-25 23:01:02 +0900

    20251225

---
+++ Chap02_DataType/Q04/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args){
+		char c = 89;
+		System.out.println(c);
+	}
+}
+
+
+
---
+++ Chap02_DataType/Q07/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String[]  args){
+		Sample s = new Sample("sample");
+		s.test();
+	}
+}
+
+
