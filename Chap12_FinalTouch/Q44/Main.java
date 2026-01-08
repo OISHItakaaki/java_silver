55e31b351af27d1a9da68ec67932a89cb9ee6ab1
大石昂明 <takakaki.ohishi1109@gmail.com>
2026-01-08 21:14:59 +0900

    20260108

---
+++ Chap12_FinalTouch/Q44/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args){
+		SubSample s1 = new SubSample(100);
+		SubSamele s2 = new SubSample("sample", 200, 100);
+		System.out.println(s1.name + ", " + s1.num + ", " + s1.price);
+		System.out.println(s2.name + ", " + s2.num + ", " + s2.price);
+	}
+}
+
