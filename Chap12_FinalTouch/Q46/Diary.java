9e1b80cdbb02fd2f012d706f53ac29fcb58fa2fc
大石昂明 <takakaki.ohishi1109@gmail.com>
2026-01-19 21:39:36 +0900

    20260119

---
+++ Chap12_FinalTouch/Q46/Diary.java
@@ -0,0 +1,11 @@
+
+import java.time.LocalDate;
+
+public class Diary {
+	private LocalDate now = LocalDate.now();
+	public LocalDate getNow() {
+		return this.now;
+	}
+}
+
+
---
+++ Chap12_FinalTouch/Q46/Main.java
@@ -0,0 +1,12 @@
+ 
+public class Main {
+	public static void main (String[]  args){
+		Diary d = new Diary();
+		System.out.println(d.getNow());
+	}
+}
+
+
+
+
+
