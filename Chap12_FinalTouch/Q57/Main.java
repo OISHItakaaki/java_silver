ff2c475506958968fb0bb1f53d0a8a91e43ab7b9
大石昂明 <takakaki.ohishi1109@gmail.com>
2026-01-21 20:31:02 +0900

    20260121

---
+++ Chap12_FinalTouch/Q57/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[]  args){
+		try {
+			throw new Exception();
+		} catch (Exception e) {
+			throw new RuntimeException();
+		} catch (RuntimeException e) {
+			System.out.println("A");
+		} finally {
+			System.out.println("B");
+		}
+	}
+}
+
+
+
+
